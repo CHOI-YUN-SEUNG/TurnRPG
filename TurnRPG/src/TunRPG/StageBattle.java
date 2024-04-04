f31ad5b53a9c64dd12f44d6135c13ea2b8d86b5a
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 17:02:09 +0900

    add temp methods

--- TurnRPG/src/TunRPG/StageBattle.java
+++ TurnRPG/src/TunRPG/StageBattle.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 public class StageBattle extends Stage {
 	private UnitManager unitManager = UnitManager.instance;
 	private Vector<Monster> monList = null;
-	private Random ran = new Random();
+	private Random rNum = new Random();
 	private int monDead = 0;
 	private int playerDead = 0;
 
@@ -20,7 +20,6 @@ public class StageBattle extends Stage {
 		while (run) {
 			if (turn) {
 				print_character();
-				// 플레이어 수만큼 공격 가능
 				if (p_index < Player.getPartySize()) {
 					player_attack(p_index);
 					p_index += 1;
@@ -29,7 +28,6 @@ public class StageBattle extends Stage {
 					p_index = 0;
 				}
 			} else if (!turn) {
-				// 몬스터 수만큼 공격 가능
 				if (m_index < monList.size()) {
 					monster_attack(m_index);
 					m_index += 1;
@@ -49,84 +47,89 @@ public class StageBattle extends Stage {
 
 	@Override
 	public void init() {
-
-//		unitManager.mon_list.clear();
-//		unitManager.monster_rand_set(4);
-//		unitManager.player = null;
-//		unitManager.player = new Player();
-//		monList = null;
-//		monList = um.mon_list;
-//		monDead = monList.size();
-//		playerDead = Player.getGuildSize();
+		unitManager.mon_list.clear();
+		unitManager.monster_rand_set(4);
+		unitManager.player = new Player();
+		monList = unitManager.mon_list;
+		monDead = monList.size();
+		playerDead = Player.getGuildSize();
 
 	}
 
-	void print_character() {
-//		System.out.println("======[BATTLE]======");
-//		System.out.println(playerDead + " : " + monDead);
-//		System.out.println("======[PLAYER]======");
-//		for (int i = 0; i < Player.getGuildSize(); i++) {
-//			Player.getGuildUnit(i).printData();
-//		}
-//		System.out.println("======[MONSTER]======");
-//		for (int i = 0; i < monList.size(); i++) {
-//			monList.get(i).printData();
-//		}
+	private void print_character() {
+		System.out.println("======[BATTLE]======");
+		System.out.println(playerDead + " : " + monDead);
+		System.out.println("======[PLAYER]======");
+		for (int i = 0; i < Player.getGuildSize(); i++) {
+			Player.getGuildUnit(i).printData();
+		}
+		System.out.println("======[MONSTER]======");
+		for (int i = 0; i < monList.size(); i++) {
+			monList.get(i).printData();
+		}
 	}
 
-	void player_attack(int index) {
-//		Player p = Player.getGuildUnit(index);
-//
-//		if (p.hp <= 0)
-//			return;
-//
-//		System.out.println("======[메뉴 선택]=====");
-//		System.out.println("[" + p.name + "] [1.어택] [2.스킬]");
-//
-//		int sel = GameManager.sc.nextInt();
-//		if (sel == 1) {
-//			while (true) {
-//				// 몬스터들 중 한마리에게 랜덤 공격
-//				int idx = r.nextInt(monList.size());
-//				if (monList.get(idx).curhp > 0) {
-//					p.attack(monList.get(idx));
-//					break;
-//				}
-//			}
-//		} else if (sel == 2) {
-//
-//		}
+	private void player_attack(int index) {
+		Player p = Player.getGuildUnit(index);
+
+		if (p.hp <= 0)
+			return;
+
+		System.out.println("======[메뉴 선택]=====");
+		System.out.println("[" + p.name + "] [1.어택] [2.스킬]");
+
+		int sel = GameManager.scanner.nextInt();
+		if (sel == 1) {
+			while (true) {
+				int idx = rNum.nextInt(monList.size());
+				if (monList.get(idx).getCurhp() > 0) {
+					p.attack(monList.get(idx));
+					break;
+				}
+			}
+		} else if (sel == 2) {
+			while (true) {
+				int idx = rNum.nextInt(monList.size());
+				if (monList.get(idx).getCurhp() > 0) {
+					p.skill(monList.get(idx));
+					break;
+				}
+			}
+		}
 	}
 
-	void monster_attack(int index) {
-//		Monster m = monList.get(index);
-//		if (m.curhp <= 0)
-//			return;
-//		while (true) {
-//			int idx = r.nextInt(Player.getGuildSize());
-//			if (Player.getGuildUnit(idx).hp > 0) {
-//				m.attack(Player.getGuildUnit(idx));
-//				break;
-//			}
-//		}
+	private void monster_attack(int index) {
+		Monster m = monList.get(index);
+		if (m.getCurhp() <= 0)
+			return;
+		while (true) {
+			int idx = rNum.nextInt(Player.getGuildSize());
+			if (Player.getGuildUnit(idx).hp > 0) {
+				m.attack(Player.getGuildUnit(idx));
+				break;
+			}
+		}
 	}
 
-	void check_live() {
-//		int num = 0;
-//		for (int i = 0; i < Player.getGuildSize(); i++) {
-//			if (Player.getGuildUnit(i).hp <= 0) {
-//				num += 1;
-//			}
-//		}
-//		playerDead = Player.getGuildSize() - num;
-//
-//		num = 0;
-//		for (int i = 0; i < monList.size(); i++) {
-//			if (monList.get(i).curhp <= 0) {
-//				num += 1;
-//			}
-//		}
-//		monDead = monList.size() - num;
+	public int getPlayerDead() {
+		return playerDead;
 	}
+	
+	private void check_live() {
+		int num = 0;
+		for (int i = 0; i < Player.getGuildSize(); i++) {
+			if (Player.getGuildUnit(i).hp <= 0) {
+				num += 1;
+			}
+		}
+		playerDead = Player.getGuildSize() - num;
 
+		num = 0;
+		for (int i = 0; i < monList.size(); i++) {
+			if (monList.get(i).getCurhp() <= 0) {
+				num += 1;
+			}
+		}
+		monDead = monList.size() - num;
+	}
 }
