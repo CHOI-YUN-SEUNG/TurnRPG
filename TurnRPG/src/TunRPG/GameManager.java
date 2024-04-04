96030d1b388723cd5bfdc5838b84db4626e1a067
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 11:15:39 +0900

    Create class GameManager

---
+++ TurnRPG/src/TunRPG/GameManager.java
@@ -0,0 +1,18 @@
+package TunRPG;
+
+public class GameManager {
+
+	static GameManager instance = new GameManager();
+
+	public void init() {
+		
+		
+	}
+
+	public boolean changeStage() {
+		return false;
+	}
+	
+	
+	
+}
