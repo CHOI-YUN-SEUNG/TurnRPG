49f186d10e5111dbf7ce540e8b6b7d3c95d70b11
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 17:05:53 +0900

    add todo message

--- TurnRPG/src/TunRPG/UnitManager.java
+++ TurnRPG/src/TunRPG/UnitManager.java
@@ -9,8 +9,9 @@ public class UnitManager {
 	Vector<Player> player_list = new Vector<Player>();
 	Vector<Monster> mon_list = new Vector<>();
 	String pate = "TunRPG.";
-	String mons[] = { "MonsterTest" };
-
+	String mons[] = { "MonsterTest" }; 
+	//mon 배열은 맵으로 부터 받아오는걸로 변경해야 맵 테마에 맞는 몹을 소환하기 편할것같다
+	//미믹은 그냥 따로 메소드를 만드는게 편할 것 같다
 	Random r = new Random();
 	Player player = new Player();
 
