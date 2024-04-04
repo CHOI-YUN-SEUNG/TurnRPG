13cfb09c7e93c9c2e3e0ac1ecdd68c3f13e4ee80
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 12:43:54 +0900

    Create class MonsterTest

--- TurnRPG/src/TunRPG/Monster.java
+++ TurnRPG/src/TunRPG/Monster.java
@@ -1,10 +1,25 @@
 package TunRPG;
 
 public class Monster {
+	int curhp;
+	int maxhp;
+	int power;
+	String name;
+	String state = "노말";
+
+	Monster() {
+	}
+
+	Monster(String name, int max, int pw) {
+		this.name = name;
+		this.curhp = max;
+		this.maxhp = max;
+		this.power = pw;
+	}
 
 	public void init(int hp, int pw) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
---
+++ TurnRPG/src/TunRPG/MonsterTest.java
@@ -0,0 +1,11 @@
+package TunRPG;
+
+public class MonsterTest extends Monster {
+	MonsterTest() {
+		name = "테스트 몬스터";
+	}
+
+	void skill() {
+		System.out.println("테스트몬스터입니다");
+	}
+}
