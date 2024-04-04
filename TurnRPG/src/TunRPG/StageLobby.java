b181bbb63b298084568bc9b1ead2bb732df13b24
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 11:44:44 +0900

    Create class StageLobby

---
+++ TurnRPG/src/TunRPG/StageLobby.java
@@ -0,0 +1,29 @@
+package TunRPG;
+
+
+public class StageLobby extends Stage {
+
+	@Override
+	public boolean update() {
+		System.out.println("=====[LOBBY]=====");
+		System.out.println("[1. 전투] [2. 설정] [3. 종료]");
+		int sel = -1;
+		while (0 > sel || sel > 2)
+			sel = GameManager.inputNumber("입력>> ");
+
+		if (sel == 1) {
+			GameManager.nextStage = "BATTLE";
+		} else if (sel == 2) {
+			GameManager.nextStage = "SETTING";
+		} else if (sel == 3) {
+			GameManager.nextStage = "";
+		}
+		return false;
+	}
+
+	@Override
+	public void init() {
+		UnitManager.instance.init();
+
+	}
+}
