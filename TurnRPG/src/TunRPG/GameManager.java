ce3e01073074942a6a7f24cdf885ca2ea2548bdf
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-04 17:05:20 +0900

    fix typo

--- TurnRPG/src/TunRPG/GameManager.java
+++ TurnRPG/src/TunRPG/GameManager.java
@@ -11,7 +11,6 @@ public class GameManager {
 
 	private String curStage = "";
 	private Map<String, Stage> stageList = new HashMap<String, Stage>();
-	private int turn;
 
 	private GameManager() {
 
@@ -24,7 +23,6 @@ public class GameManager {
 		stageList.put("BONUS", new StageBonus());
 		stageList.put("SETTING", new StageSetting());
 		nextStage = "TITLE";
-		turn = 0;
 	}
 
 	public boolean changeStage() {
