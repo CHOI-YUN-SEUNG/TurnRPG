d804440898a3e948d2abc9e3a845b7c48228c817
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-05 18:38:38 +0900

    modify equipMenu(),inventoryMenu(), printItemList() method

--- TurnRPG/src/TurnRPG/Inventory.java
+++ TurnRPG/src/TurnRPG/Inventory.java
@@ -3,61 +3,57 @@ package TurnRPG;
 import java.util.ArrayList;
 
 public class Inventory {
+	private ArrayList<Item> itemList = new ArrayList<>();
 
-	ArrayList<Item> itemList = new ArrayList<>();
-
-	// 인벤토리 메뉴 메서드
 	public void inventoryMenu() {
 		while (true) {
 			System.out.println("============ [인벤토리 메뉴] =============");
-			System.out.println("[1.착용] [2.판매] [0.뒤로가기]");
-			int sel = GameManager.scanner.nextInt();
+			int sel = -1;
+
+			while (!(sel >= 0 && sel < 3))
+				sel = GameManager.inputNumber("[1.착용] [2.판매] [0.뒤로가기]");
+
 			if (sel == 0)
 				break;
-			if (sel == 1) {
+			else if (sel == 1)
 				equipMenu();
-			}
-			if (sel == 2) {
+			else if (sel == 2)
 				sellMenu();
-			}
 		}
 	}
 
-	// 장비 착용 메서드
 	public void equipMenu() {
-		// 길드원 능력값 출력
 		Player.guild.printAllUnitStaus();
-		System.out.println("아이템 착용할 길드원을 선택하세요 ");
-		int selUnit = GameManager.scanner.nextInt() - 1;
+		int selUnit = -1;
+		while (!(selUnit >= 0 && selUnit < Player.getGuildList().size()))
+			selUnit = GameManager.inputIndex("아이템 착용할 길드원을 선택하세요");
+
 		while (true) {
 			Player.guild.printUnitStaus(selUnit);
 			Player.guild.printUnitItem(selUnit);
 			printItemList();
 
-			System.out.println("착용할 아이템 번호를 입력하세요 [0.뒤로가기]");
-			int selEquip = GameManager.scanner.nextInt();
-			if (selEquip == 0)
+			int selEquip = GameManager.inputIndex("착용할 아이템 번호를 입력하세요 [0.뒤로가기]");
+			if (selEquip == -1)
 				break;
-			selEquip -= 1;
 
-			// 입력한 아이템의 종류가 무기면
-			if (itemList.get(selEquip).getKind() == Item.WEAPON) {
-				// 길드원에게 무기가 없으면 아이템리스트에 추가 후 무기 착용
+			if (itemList.size() > selEquip) {
+				System.out.println("해당하는 번호의 아이템이 없습니다.");
+			} else if (itemList.get(selEquip).getKind() == Item.WEAPON) {
 				if (Player.getGuildUnit(selUnit).getWeapon() != null) {
 					itemList.add(Player.getGuildUnit(selUnit).getWeapon());
 				}
-				Player.getGuildUnit(selUnit).setWeapon(itemList.get(selEquip)); 
-			} // 입력한 아이템의 종류가 갑옷이면
-			else if (itemList.get(selEquip).getKind() == Item.ARMOR) {
-				// 길드원에게 무기가 없으면 아이템리스트에 추가 후 무기 착용
-				if (Player.getGuildUnit(selUnit).getArmor() != null) {
-					itemList.add(Player.getGuildUnit(selUnit).getArmor());
+				Player.getGuildUnit(selUnit).setWeapon(itemList.get(selEquip));
+
+			} else if (itemList.get(selEquip).getKind() == Item.PROTECTION) {
+				if (Player.getGuildUnit(selUnit).getProtection() != null) {
+					itemList.add(Player.getGuildUnit(selUnit).getProtection());
 				}
-				Player.getGuildUnit(selUnit).setArmor(itemList.get(selEquip));
-			} // 입력한 아이템의 종류가 반지면
-			else if (itemList.get(selEquip).getKind() == Item.RING) {
+				Player.getGuildUnit(selUnit).setProtection(itemList.get(selEquip));
+
+			} else if (itemList.get(selEquip).getKind() == Item.RING) {
 				if (Player.getGuildUnit(selUnit).getRing() != null) {
-					itemList.add(Player.getGuildUnit(selUnit).getArmor());
+					itemList.add(Player.getGuildUnit(selUnit).getRing());
 				}
 				Player.getGuildUnit(selUnit).setRing(itemList.get(selEquip));
 			}
@@ -65,13 +61,28 @@ public class Inventory {
 		}
 	}
 
-
 	public void printItemList() {
 		System.out.println("============ [아이템리스트] ==============");
 		for (int i = 0; i < itemList.size(); i++) {
 			System.out.print("[" + (i + 1) + "번] ");
 			System.out.print("[이름 : " + itemList.get(i).getName() + "] ");
-		//	System.out.print("[능력 : " + itemList.get(i).getPower() + "] ");
+			System.out.print("[능력 : ");
+			if (itemList.get(i).getAtt() != 0) {
+				System.out.print("공격력 +" + itemList.get(i).getAtt());
+			}
+			if (itemList.get(i).getHp() != 0) {
+				System.out.print(" 체력 +" + itemList.get(i).getHp());
+			}
+			if (itemList.get(i).getDef() != 0) {
+				System.out.print(" 방어력 +" + itemList.get(i).getDef());
+			}
+			if (itemList.get(i).getDex() != 0) {
+				System.out.print(" 회피력 +" + itemList.get(i).getDex());
+			}
+			if (itemList.get(i).getCri() != 0) {
+				System.out.print(" 치명타확률 +" + itemList.get(i).getCri());
+			}
+			System.out.print("] ");
 			System.out.print("[가격 : " + itemList.get(i).getPrice() + "] ");
 			System.out.println();
 		}
@@ -93,12 +104,15 @@ public class Inventory {
 				e.printStackTrace();
 			}
 			Player.money += (itemList.get(selSell).getPrice() / 2); // 50% 세금
-			itemList.remove(selSell); // 아이템리스트에서 삭제
+			itemList.remove(selSell);
 		}
 	}
 
-	// 아이템 추가 메서드
-	void addItem(Item item) {
+	public ArrayList<Item> getItemList() {
+		return itemList;
+	}
+
+	public void addItem(Item item) {
 		itemList.add(item);
 	}
 
