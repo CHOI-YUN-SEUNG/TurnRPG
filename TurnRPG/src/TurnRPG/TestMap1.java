67b04d3b0c0eebeda2d2366ece942887c54069e5
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-09 11:44:03 +0900

    add spawnMonsters() and Override  getName(), enerateSubMaps().

--- TurnRPG/src/TurnRPG/TestMap1.java
+++ TurnRPG/src/TurnRPG/TestMap1.java
@@ -1,15 +1,31 @@
 package TurnRPG;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMap1 extends GameMap {
+	private UnitManager unitManager = UnitManager.instance;
 
 	public TestMap1(String name, boolean visitable) {
 		super(name, visitable);
 	}
 
-	// 세부지역 이름을 다시 짓고 싶은데
-	
-	
-	
-	
-	
+	@Override
+	public String getName() {
+		return "고블린 부락";
+	}
+
+	@Override
+	public List<String> generateSubMaps() {
+		List<String> subMaps = new ArrayList<>();
+		subMaps.add("테스트 지역 A");
+		subMaps.add("테스트 지역 B");
+		subMaps.add("테스트 지역 C");
+		return subMaps;
+	}
+
+	public void spawnMonsters() {
+		unitManager.spawnMonsters(this.getName(), 3); 
+		unitManager.spawnMonsters(this.getName(), 2); 
+	}
 }
