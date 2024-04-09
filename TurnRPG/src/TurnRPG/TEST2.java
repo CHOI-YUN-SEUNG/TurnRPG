c8874e89dd5b493d62c7b5b461ecc047f4ede7ff
CHOI-YUN-SEUNG <ryans03@naver.com>
2024-04-09 11:39:48 +0900

    Create class TEST2 for battle test

---
+++ TurnRPG/src/TurnRPG/TEST2.java
@@ -0,0 +1,11 @@
+package TurnRPG;
+
+public class TEST2  extends Monster {
+	public TEST2() {
+		setName("테스트 몬스터2");
+	}
+	
+	public void skill() {
+		System.out.println("테스트몬스터2가 스킬을 쓰는 척했다!");
+	}
+}
